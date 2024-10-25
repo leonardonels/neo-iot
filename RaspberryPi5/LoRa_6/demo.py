95f7eebae410bb428f7f6132a6f055b607ce6e5e
leonardonels <goldenmatrix2.0@gmail.com>
2024-10-25 19:40:56 +0200

    debug 6 the presiquel

---
+++ RaspberryPi5/LoRa_6/demo.py
@@ -0,0 +1,77 @@
+import spidev
+from gpiozero import OutputDevice, InputDevice
+from time import sleep
+
+# Pin configuration
+RST_PIN = 22
+DIO0_PIN = 27
+CS_PIN = 25
+
+# LoRa register addresses (consts)
+REG_FIFO = 0x00
+REG_OP_MODE = 0x01
+REG_PA_CONFIG = 0x09
+REG_FRF_MSB = 0x06
+REG_FRF_MID = 0x07
+REG_FRF_LSB = 0x08
+REG_MODEM_CONFIG1 = 0x1D
+REG_MODEM_CONFIG2 = 0x1E
+REG_MODEM_CONFIG3 = 0x26
+REG_PAYLOAD_LENGTH = 0x22
+REG_FIFO_TX_BASE_ADDR = 0x0E
+REG_FIFO_RX_BASE_ADDR = 0x0F
+REG_FIFO_ADDR_PTR = 0x0D
+REG_IRQ_FLAGS = 0x12
+REG_VERSION = 0x42
+
+# LoRa operational modes
+MODE_SLEEP = 0x00
+MODE_STDBY = 0x01
+MODE_TX = 0x83
+MODE_LORA = 0x80
+
+# LoRa configuration values
+MAX_POWER = 0xF0
+BANDWIDTH_500KHZ = 0x72
+SPREADING_FACTOR_7 = 0x74
+CODING_RATE_4_5 = 0x04
+
+# MSB settings
+MSB_1 = 0x80
+MSB_0 = 0x7F
+
+# GPIO setup using gpiozero
+reset_pin = OutputDevice(RST_PIN, active_high=False)
+dio0_pin = InputDevice(DIO0_PIN)
+cs_pin = OutputDevice(CS_PIN, active_high=False)
+
+# SPI setup
+spi = spidev.SpiDev()
+spi.open(0, 0)
+spi.max_speed_hz = 500000   #500Hz
+
+#...
+# Function to write on LoRa register
+def write_register(address, data):
+    cs_pin.on()
+    spi.xfer([address | MSB_1, data])
+    cs_pin.off()
+
+# Function to read from LoRa register
+def read_register(address):
+    cs_pin.on()
+    response = spi.xfer([address & MSB_0, 0x00])
+    cs_pin.off()
+    return response[1]
+
+def check_mode():
+    current_mode = read_register(REG_OP_MODE)
+    print(f"Current mode: {current_mode}")
+
+try:
+    while True:
+        print("starting demo")
+        check_mode()
+        sleep(5)
+except KeyboardInterrupt:
+    spi.close()
