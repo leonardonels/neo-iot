ca0fcb63f8394eae434482e9c5a1c7fc6840fde5
leonardonels <goldenmatrix2.0@gmail.com>
2024-10-26 23:33:35 +0200

    boh

--- Arduino/LoRa_4/LoRa_4.ino
+++ Arduino/LoRa_4/LoRa_4.ino
@@ -1,35 +1,36 @@
+#include <SPI.h>
 #include <LoRa.h>
+
+// Configurazione pin (modifica se necessario)
+const int csPin = 10;    // NSS
+const int resetPin = 9;  // RESET
+const int dioPin = 2;    // DIO0
 
 void setup() {
   Serial.begin(9600);
+  while (!Serial);
 
-  LoRa.setPins(10, 2, 9); // NSS, DIO0, Reset
-  
-  // Inizializza LoRa alla frequenza desiderata (es. 433E6 per 433 MHz)
-  if (!LoRa.begin(433E6)) {
-    Serial.println("Errore di inizializzazione LoRa!");
-    while (1);
+  // Configurazione pin
+  LoRa.setPins(csPin, resetPin, dioPin);
+
+  // Inizializzazione modulo LoRa
+  if (!LoRa.begin(433E6)) { // Frequenza 433 MHz, assicurati che sia la stessa del trasmettitore
+    Serial.println("LoRa init failed. Check your connections.");
+    while (true);
   }
-
-  // Imposta la banda a 500 kHz
-  LoRa.setSignalBandwidth(500E3);
   
-  // Imposta lo Spreading Factor a 7
-  LoRa.setSpreadingFactor(7);
-  
-  // Imposta il Coding Rate a 4/5
-  LoRa.setCodingRate4(5);
+  LoRa.setSpreadingFactor(7);        // Spreading Factor SF7
+  LoRa.setSignalBandwidth(500E3);    // Banda 500 kHz
+  LoRa.setCodingRate4(5);            // Coding Rate 4/5
 
-  Serial.println("LoRa inizializzato con successo!");
+  Serial.println("LoRa Transmitter initialized.");
 }
 
 void loop() {
-  Serial.println("Hello, world!");
-
-  // Esempio di invio messaggio
   LoRa.beginPacket();
-  LoRa.print("Hello, world!");
+  LoRa.print("Hello world!");
   LoRa.endPacket();
+  Serial.println("Hello wrold!");
   
   delay(5000);  // Ritardo tra i pacchetti
 }
--- Arduino/LoRa_8a/transmitter/transmitter.ino
+++ Arduino/LoRa_8a/transmitter/transmitter.ino
@@ -1,32 +1,53 @@
+#include <SPI.h>
 #include <LoRa.h>
 
+//define the pins used by the transceiver module
+#define NSS 10
+#define RST 9
+#define DIO0 2
+
+String loraString = "";
+int val = 0;
+
 void setup() {
+  //Initialize the serial monitor
   Serial.begin(9600);
-  
-  // Inizializza LoRa alla frequenza desiderata (es. 433E6 per 433 MHz)
+  while (!Serial);
+
+  Serial.println("LoRa Receiver");
+
+   //setup LoRa NSS, RST and DIO0 pins for transceiver module
+  LoRa.setPins(NSS, RST, DIO0);
+
+  // select the frequency according to your module. most commom frequences are 433E6,866E6 and 915E6
   if (!LoRa.begin(433E6)) {
-    Serial.println("Errore di inizializzazione LoRa!");
+    Serial.println("Starting LoRa failed!");
     while (1);
   }
 
-  // Imposta la banda a 500 kHz
-  LoRa.setSignalBandwidth(500E3);
-  
-  // Imposta lo Spreading Factor a 7
-  LoRa.setSpreadingFactor(7);
-  
-  // Imposta il Coding Rate a 4/5
-  LoRa.setCodingRate4(5);
-
-  Serial.println("LoRa inizializzato con successo!");
+  // Change sync word (0xF3) to match the receiver
+  // This is a unique message id. The sync word use to assures taht LoRa messages don't match to other LoRa transceivers
+  // 0-0xFF range
+  LoRa.setSyncWord(0xF3);
+  Serial.println("LoRa Initializing OK!");
 }
 
 void loop() {
-  // Esempio di invio messaggio
-  LoRa.beginPacket();
-  LoRa.print("Hello world!");
-  LoRa.endPacket();
-  Serial.println("Hello world!");
-  
-  delay(5000);  // Ritardo tra i pacchetti
+  // try to parse packet
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    // received a packet
+    Serial.print("Received packet '");
+
+    // read packet
+    while (LoRa.available()) {
+      // read the message from lora transmitter
+      Serial.print((char)LoRa.read());
+    }
+    Serial.println();
+
+    // print RSSI of packet
+    Serial.print("' with RSSI ");
+    Serial.println(LoRa.packetRssi());
+  }
 }
