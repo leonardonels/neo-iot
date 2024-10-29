71682b037a218a266b1f2011c103739021d0bc58
leonardonels <goldenmatrix2.0@gmail.com>
2024-10-29 10:30:19 +0100

    fix 9

--- RaspberryPi5/LoRa_8/trasmitter.py
+++ RaspberryPi5/LoRa_8/trasmitter.py
@@ -202,11 +202,12 @@ def send(message):
     write_register(REG_PAYLOAD_LENGTH, 0x00)
     print(f"payload_len to 0: {check(REG_PAYLOAD_LENGTH)}")
 
+    payload_length = 0
     for byte in message.encode():
         write_register(REG_FIFO, byte)
+        payload_length+=1
     print("Message written to FIFO")
 
-    payload_length = len(message)+5
     write_register(REG_PAYLOAD_LENGTH, payload_length)
     print(f"payload_len {payload_length}: {check(REG_PAYLOAD_LENGTH)}")
 
