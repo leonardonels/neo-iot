daf67446a1b90e544a481e982c810b4ccf2487a0
leonardonels <goldenmatrix2.0@gmail.com>
2024-11-08 17:16:50 +0100

    trasmitter added

---
+++ RaspberryPi5/LoRa_8b/trasmitter.py
@@ -0,0 +1,29 @@
+import LoRa as lora
+from time import sleep
+
+# Pin configuration
+RST_PIN                     = 22
+DIO0_PIN                    = 27
+CS_PIN                      = 25
+
+#SPI configuration
+SPI_FREQUENCY               = 8000000 # 8 MHz
+
+#LoRa configuration
+FREQUENCY                   = 433  # MHz
+F_XOSC                      = 32   # MHz
+BANDWIDTH                   = 0x90 # 500 Hz
+SPREADING_FACTOR            = 0x70 # 7
+CODING_RATE                 = 0x02 # 4/5
+
+try:
+    lora.setup(CS_PIN, RST_PIN, DIO0_PIN, SPI_FREQUENCY)
+
+    lora.begin(frequency=FREQUENCY, hex_bandwidth=BANDWIDTH, hex_spreading_factor=SPREADING_FACTOR, hex_coding_rate=CODING_RATE)
+
+    while True:
+        lora.send("hello")
+        sleep(5)
+
+except KeyboardInterrupt:
+    lora.close()
