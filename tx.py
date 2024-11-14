48c348fab32c4e3cd6d21cf832ecfad7295ab0dd
leonardonels <goldenmatrix2.0@gmail.com>
2024-11-15 00:06:08 +0100

    enogh for now

--- tx.py
+++ tx.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 import sys
 from time import sleep
-from SX127x.LoRa import *
-from SX127x.LoRaArgumentParser import LoRaArgumentParser
-from SX127x.board_config import BOARD
+import SX127x.LoRa as lora
+from SX127x.constants import *
 import LoRaWAN
 from LoRaWAN.MHDR import MHDR
 
-BOARD.setup()
-parser = LoRaArgumentParser("LoRaWAN sender")
+# Pin configuration
+RST_PIN                     = 22
+DIO0_PIN                    = 27
+CS_PIN                      = 25
 
-class LoRaWANsend(LoRa):
+#SPI configuration
+SPI_FREQUENCY               = 8000000 # 8 MHz
+
+#LoRa configuration
+FREQUENCY                   = 433  # MHz
+F_XOSC                      = 32   # MHz
+BANDWIDTH                   = 0x90 # 500 Hz
+SPREADING_FACTOR            = 0x70 # 7
+COD_RATE                    = 0x02 # 4/5
+
+class LoRaWANsend():
     def __init__(self, devaddr = [], nwkey = [], appkey = [], verbose = False):
         super(LoRaWANsend, self).__init__(verbose)
         self.devaddr = devaddr
@@ -37,28 +48,19 @@ class LoRaWANsend(LoRa):
 devaddr = [0x26, 0x01, 0x11, 0x5F]
 nwskey = [0xC3, 0x24, 0x64, 0x98, 0xDE, 0x56, 0x5D, 0x8C, 0x55, 0x88, 0x7C, 0x05, 0x86, 0xF9, 0x82, 0x26]
 appskey = [0x15, 0xF6, 0xF4, 0xD4, 0x2A, 0x95, 0xB0, 0x97, 0x53, 0x27, 0xB7, 0xC1, 0x45, 0x6E, 0xC5, 0x45]
-lora = LoRaWANsend(False)
 
-# Setup
-lora.set_mode(MODE.SLEEP)
-lora.set_dio_mapping([1,0,0,0,0,0])
-lora.set_freq(868.1)
-lora.set_pa_config(pa_select=1)
-lora.set_spreading_factor(7)
-lora.set_pa_config(max_power=0x0F, output_power=0x0E)
-lora.set_sync_word(0x34)
-lora.set_rx_crc(True)
-
-print(lora)
-assert(lora.get_agc_auto_on() == 1)
 
 try:
+    lora.setup(CS_PIN, RST_PIN, DIO0_PIN, SPI_FREQUENCY, debug=True)
+    lora.begin(frequency=FREQUENCY, hex_bandwidth=BANDWIDTH, hex_spreading_factor=SPREADING_FACTOR, hex_coding_rate=COD_RATE, rx_crc=True)
+
+    lorawan=LoRaWANsend()
     print("Sending LoRaWAN message\n")
-    lora.start()
+    lorawan.start()
 except KeyboardInterrupt:
     sys.stdout.flush()
     print("\nKeyboardInterrupt")
 finally:
     sys.stdout.flush()
     lora.set_mode(MODE.SLEEP)
-    BOARD.teardown()
+    lora.close()
