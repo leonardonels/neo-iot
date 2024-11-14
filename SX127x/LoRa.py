5378b2c6721dd8448b91ad4f593530f3627c01ec
leonardonels <goldenmatrix2.0@gmail.com>
2024-11-14 22:56:04 +0100

    chaged freq from 868 to 433

--- SX127x/LoRa.py
+++ SX127x/LoRa.py
@@ -81,7 +81,7 @@ class LoRa(object):
     verbose = True
     dio_mapping = [None] * 6          # store the dio mapping here
 
-    def __init__(self, verbose=True, do_calibration=True, calibration_freq=868):
+    def __init__(self, verbose=True, do_calibration=True, calibration_freq=433):
         """ Init the object
         
         Send the device to sleep, read all registers, and do the calibration (if do_calibration=True)
@@ -825,7 +825,7 @@ class LoRa(object):
         """
         return 0x87 if pa_dac else 0x84
 
-    def rx_chain_calibration(self, freq=868.):
+    def rx_chain_calibration(self, freq=433.):
         """ Run the image calibration (see Semtech documentation section 4.2.3.8)
         :param freq: Frequency for the HF calibration
         :return: None
