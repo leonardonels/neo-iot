ed7a4ffae898a77f5b75d604a6c623d2e5695cbc
leonardonels <goldenmatrix2.0@gmail.com>
2024-11-15 09:20:37 +0100

    lets start again

--- tx.py
+++ tx.py
@@ -1,10 +1,7 @@
-#!/usr/bin/env python3
-import sys
 from time import sleep
-import SX127x.LoRa as lora
-from SX127x.constants import *
-import LoRaWAN
-from LoRaWAN.MHDR import MHDR
+import sys
+import LoRa.LoRa as lora
+from LoRa.constants import MODE
 
 # Pin configuration
 RST_PIN                     = 22
@@ -21,29 +18,6 @@ BANDWIDTH                   = 0x90 # 500 Hz
 SPREADING_FACTOR            = 0x70 # 7
 COD_RATE                    = 0x02 # 4/5
 
-class LoRaWANsend():
-    def __init__(self, devaddr = [], nwkey = [], appkey = [], verbose = False):
-        super(LoRaWANsend, self).__init__(verbose)
-        self.devaddr = devaddr
-        self.nwkey = nwkey
-        self.appkey = appkey
-
-    def on_tx_done(self):
-        self.set_mode(MODE.STDBY)
-        self.clear_irq_flags(TxDone=1)
-        print("TxDone")
-        sys.exit(0)
-
-    def start(self):
-        lorawan = LoRaWAN.new(nwskey, appskey)
-        lorawan.create(MHDR.UNCONF_DATA_UP, {'devaddr': devaddr, 'fcnt': 1, 'data': list(map(ord, 'Python rules!')) })
-
-        self.write_payload(lorawan.to_raw())
-        self.set_mode(MODE.TX)
-        while True:
-            sleep(1)
-
-
 # Init
 devaddr = [0x26, 0x01, 0x11, 0x5F]
 nwskey = [0xC3, 0x24, 0x64, 0x98, 0xDE, 0x56, 0x5D, 0x8C, 0x55, 0x88, 0x7C, 0x05, 0x86, 0xF9, 0x82, 0x26]
@@ -54,9 +28,9 @@ try:
     lora.setup(CS_PIN, RST_PIN, DIO0_PIN, SPI_FREQUENCY, debug=True)
     lora.begin(frequency=FREQUENCY, hex_bandwidth=BANDWIDTH, hex_spreading_factor=SPREADING_FACTOR, hex_coding_rate=COD_RATE, rx_crc=True)
 
-    lorawan=LoRaWANsend()
-    print("Sending LoRaWAN message\n")
-    lorawan.start()
+    while True:
+        lora.send("Hello World! This is a very long message!")
+        sleep(5)
 except KeyboardInterrupt:
     sys.stdout.flush()
     print("\nKeyboardInterrupt")
