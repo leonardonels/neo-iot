cc1c33cda1fd4238eef97dabdf6c099bd6147ac6
leonardonels <goldenmatrix2.0@gmail.com>
2024-11-15 09:48:26 +0100

    fix 2

--- LoRaTWO/LoRaTWO.py
+++ LoRaTWO/LoRaTWO.py
@@ -2,14 +2,18 @@ from time import sleep
 from LoRa.constants import MODE, REG
 import LoRa.LoRa as lora
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.padding import PKCS7
 from os import urandom
 
 
 # AES 128
 def send(message, key):
 
+    padder = PKCS7(128).padder()
+    padded_message = padder.update(message.encode('utf-8')) + padder.finalize()
+
     iv = urandom(16)
     cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
     encryptor = cipher.encryptor()
-    ciphertext = encryptor.update(message.encode('utf-8')) + encryptor.finalize()
+    ciphertext = encryptor.update(padded_message) + encryptor.finalize()
     lora.send(iv+ciphertext)
