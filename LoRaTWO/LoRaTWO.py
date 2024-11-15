a41548fd4f3ee686e5c9ecc51aa5b7633d5e24b7
leonardonels <goldenmatrix2.0@gmail.com>
2024-11-15 09:50:48 +0100

    fix 4

--- LoRaTWO/LoRaTWO.py
+++ LoRaTWO/LoRaTWO.py
@@ -16,4 +16,4 @@ def send(message, key):
     cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
     encryptor = cipher.encryptor()
     ciphertext = encryptor.update(padded_message) + encryptor.finalize()
-    lora.send(iv+ciphertext)
+    lora.send((iv+ciphertext).decode())
