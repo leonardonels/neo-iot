5e53440952787effaad1081da692a42f6d34b665
“leonardo” <goldenmatrix2.0@gmail.com>
2024-11-26 10:34:52 +0100

    test 9

--- LoRa/LoRa.py
+++ LoRa/LoRa.py
@@ -97,11 +97,11 @@ def activity_derection():
     if read_register(REG.LORA.DIO_MAPPING_1) != 0x40:
         write_register(REG.LORA.DIO_MAPPING_1, 0x40)
     write_register(REG.LORA.OP_MODE, MODE.CAD)
+    print(read_register(REG.LORA.MODE))
+    print(read_register(REG.LORA.DIO_MAPPING_1))
     while True:
         if dio0_pin.is_active:
             return True
-        else:
-            print("looking for a preamble...")
 
 def receive(timeout):
     set_module_on_receive()
