e14d24169bc354d19a31e6e709576d8826a887a1
leonardo <goldenmatrix2.0@gmail.com>
2024-12-02 12:58:13 +0100

    debug

--- LoRa/LoRa.py
+++ LoRa/LoRa.py
@@ -100,6 +100,7 @@ def activity_derection(ret=True):
         write_register(REG.LORA.IRQ_FLAGS, 0x00)
         write_register(REG.LORA.OP_MODE, MODE.CAD)
         irqf=read_register(REG.LORA.IRQ_FLAGS)
+        print(irqf)
         if irqf&5==4:
             if ret:
                 return True
