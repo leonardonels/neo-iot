dd34458d40c26f6f11db7184cec33f332b190129
leonardo <goldenmatrix2.0@gmail.com>
2024-12-03 15:57:03 +0100

    debug 1

--- LoRa/LoRa.py
+++ LoRa/LoRa.py
@@ -102,6 +102,7 @@ def activity_derection(timeout=0):
         write_register(REG.LORA.IRQ_FLAGS, 0x00)
         write_register(REG.LORA.OP_MODE, MODE.CAD)
         if read_register(REG.LORA.IRQ_FLAGS)&5 == 5:
+            print(read_register(REG.LORA.IRQ_FLAGS))
             return True
         if (time() - start_time > timeout)&(timeout!=0):
             return False
